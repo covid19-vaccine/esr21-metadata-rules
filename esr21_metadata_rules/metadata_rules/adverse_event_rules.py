b7a17ea5152e966816e2cae5a34fcc930c47143e
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-10 10:10:44 +0200

    add adverse event rule groups

---
+++ esr21_metadata_rules/metadata_rules/adverse_event_rules.py
@@ -0,0 +1,26 @@
+from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, register, P
+from ..predicates import SubjectPredicates
+
+app_label = 'esr21_subject'
+pc = SubjectPredicates()
+
+
+@register()
+class AdverseEventRuleGroup(CrfRuleGroup):
+
+    serious_ae = CrfRule(
+        predicate=pc.func_serious_ae_required,
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.seriousadverseevent', ])
+
+    speical_interest_ae = CrfRule(
+        predicate=pc.func_special_interest_ae_required,
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.specialinterestadverseevent', ])
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.adverseevent'
