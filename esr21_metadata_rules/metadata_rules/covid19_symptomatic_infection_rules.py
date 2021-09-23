0a471b9a4b80ea1b6b1e2b6b62ba8f6f06b3e6ee
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-23 12:05:52 +0200

    update hospitalisation rule group

--- esr21_metadata_rules/metadata_rules/covid19_symptomatic_infection_rules.py
+++ esr21_metadata_rules/metadata_rules/covid19_symptomatic_infection_rules.py
@@ -1,7 +1,7 @@
-from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, register, P
 
 from edc_constants.constants import YES
-from edc_metadata_rules import CrfRule, CrfRuleGroup, register, P
+from edc_metadata import NOT_REQUIRED, REQUIRED
 
 app_label = 'esr21_subject'
 
@@ -9,8 +9,8 @@ app_label = 'esr21_subject'
 @register()
 class covid19SymptomaticInfectionsRuleGroup(CrfRuleGroup):
 
-    sars_pcr = CrfRule(
-        predicate=P('visits', 'eq', YES),
+    hospitalisation = CrfRule(
+        predicate=P('hospitalisation_visit', 'eq', YES),
         consequence=REQUIRED,
         alternative=NOT_REQUIRED,
         target_models=[f'{app_label}.hospitalisation', ])
