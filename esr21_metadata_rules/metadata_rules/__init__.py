6f360f693dd24128dfc7e4f7e14d896f12347a2b
Itumeleng Mosweu <imosweuk@gmail.com>
2021-10-08 19:26:59 +0200

    update enrollment rule groups with symptomatic infections

--- esr21_metadata_rules/metadata_rules/__init__.py
+++ esr21_metadata_rules/metadata_rules/__init__.py
@@ -1,5 +1,6 @@
 from .adverse_event_rules import AdverseEventRuleGroup
 from .covid19_symptomatic_infection_rules import covid19SymptomaticInfectionsRuleGroup
 from .pregnancy_status_rules import pregnancyStatusRuleGroup
+from .requisition_rules import SubjectVisitReqRuleGroup
 from .subject_visit_rules import SubjectVisitRuleGroup
 from .vaccination_details_rules import VaccinationDetailsRuleGroup
