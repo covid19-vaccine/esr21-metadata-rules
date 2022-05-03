94f90b69178879e30109c6ff7777ad5706a1c80b
ame diphoko <diphokoame@gmail.com>
2022-05-03 12:11:08 +0200

    minor fix

--- esr21_metadata_rules/predicates/subject_predicates.py
+++ esr21_metadata_rules/predicates/subject_predicates.py
@@ -93,7 +93,7 @@ class SubjectPredicates(PredicateCollection):
             covid19results_obj = covid19_results_model.objects.get(
                 subject_visit__subject_identifier=visit.subject_identifier,
                 subject_visit__visit_code=visit.visit_code,
-                subject_visit__visit_code_sequence=visit.visit_code
+                subject_visit__visit_code_sequence=visit.visit_code_sequence
                 )
         except covid19_results_model.DoesNotExist:
             return None
